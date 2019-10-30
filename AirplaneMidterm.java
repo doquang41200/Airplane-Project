a39cfae40459f6494c85792942a73b3d5cbede93
TeslaPrince <57016635+TeslaPrince@users.noreply.github.com>
2019-10-29 19:06:29 -0500

    Add files via upload

--- AirplaneMidterm.java
+++ AirplaneMidterm.java
@@ -4,83 +4,136 @@
 //Modified: 10-24-19 (JL)
 //Modified: 10-25-19 (JL,CG)
 //Modified: 10-26-19 (JL)
+//Modified: 10-28-19 (JL)
 package airplane.midterm;
 import java.util.Scanner;
 public class AirplaneMidterm {
     public static Scanner input = new Scanner(System.in);
+    public static Ticket userTicket[] = new Ticket[1000];
     public static void main(String[] args) {
         System.out.println("Welcome to American Airlines!");
-        //User's First Name
-        String firstName = uin("First Name:");
-        //User's Middle Initial
-        String middleInitial;
-        do{
-            middleInitial = uin("Middle Initial(if no middle name press enter):");
-        }while(middleInitial.length() > 1);
-        //User's Last Name
-        String lastName = uin("Last Name:");
-        //User's Destination
-        String destinationCity = uin("Destination City:");
-        String destinationState;
-        do{
-            destinationState = uin("Destination State(Valid 2-letter Postal Code):");
-        }while(destinationState.length() > 2 || !destinationState.equalsIgnoreCase("AL") && !destinationState.equalsIgnoreCase("AK") && !destinationState.equalsIgnoreCase("AZ") && !destinationState.equalsIgnoreCase("AR") && !destinationState.equalsIgnoreCase("CA") && !destinationState.equalsIgnoreCase("CO") && !destinationState.equalsIgnoreCase("CT") && !destinationState.equalsIgnoreCase("DE") && !destinationState.equalsIgnoreCase("DC") && !destinationState.equalsIgnoreCase("FL") && !destinationState.equalsIgnoreCase("GA") && !destinationState.equalsIgnoreCase("HI") && !destinationState.equalsIgnoreCase("ID") && !destinationState.equalsIgnoreCase("KS") && !destinationState.equalsIgnoreCase("KY") && !destinationState.equalsIgnoreCase("LA") && !destinationState.equalsIgnoreCase("ME") && !destinationState.equalsIgnoreCase("MD") && !destinationState.equalsIgnoreCase("MA") && !destinationState.equalsIgnoreCase("MI") && !destinationState.equalsIgnoreCase("MN") && !destinationState.equalsIgnoreCase("MS") && !destinationState.equalsIgnoreCase("MO") && !destinationState.equalsIgnoreCase("MT") && !destinationState.equalsIgnoreCase("NE") && !destinationState.equalsIgnoreCase("NV") && !destinationState.equalsIgnoreCase("NH") && !destinationState.equalsIgnoreCase("NJ") && !destinationState.equalsIgnoreCase("NM") && !destinationState.equalsIgnoreCase("NY") && !destinationState.equalsIgnoreCase("NC") && !destinationState.equalsIgnoreCase("NC") && !destinationState.equalsIgnoreCase("ND") && !destinationState.equalsIgnoreCase("OH") && !destinationState.equalsIgnoreCase("OK") && !destinationState.equalsIgnoreCase("OR") && !destinationState.equalsIgnoreCase("PA") && !destinationState.equalsIgnoreCase("RI") && !destinationState.equalsIgnoreCase("SC") && !destinationState.equalsIgnoreCase("SD") && !destinationState.equalsIgnoreCase("TN") && !destinationState.equalsIgnoreCase("TX") && !destinationState.equalsIgnoreCase("UT") && !destinationState.equalsIgnoreCase("VT") && !destinationState.equalsIgnoreCase("VA") && !destinationState.equalsIgnoreCase("WA") && !destinationState.equalsIgnoreCase("WV") && !destinationState.equalsIgnoreCase("WI") && !destinationState.equalsIgnoreCase("WY"));
-        //User's Class Choice
-        int classChoice = 0;
-        String strClassChoice;
-        do{
-            strClassChoice = uin("Class:\n1. First\n2. Economy\n");
-            classChoice = tryStrToInt(strClassChoice);
-        }while(classChoice < 1 || classChoice > 2);
-        String seat;
-        switch(classChoice){
-            case 1:
-                strClassChoice = "First Class";
-                seat = firstSeat();
-                break;
-            case 2:
-                strClassChoice = "Economy Class";
-                seat = economySeat();
-                break;
-            default:
-                strClassChoice = "Economy Class";
-                seat = economySeat();
-                break;
+        int ticketNum = 1;
+        String strNumUserTickets = uin("how many tickets you would like to purchase?:");
+        int numUserTickets = tryStrToInt(strNumUserTickets);
+        for(int i = 0; i < numUserTickets; i++){
+            userTicket[i] = new Ticket();
+            //Current Ticket Number
+            userTicket[i].ticketNum = ticketNum;
+            //User's First Name
+            userTicket[i].firstName = uin("First Name:");
+            //User's Middle Initial
+            do{
+                userTicket[i].middleInitial = uin("Middle Initial(if no middle name press enter):");
+            }while(userTicket[i].middleInitial.length() > 1);
+            //User's Last Name
+            userTicket[i].lastName = uin("Last Name:");
+            //User's Age
+            String strAge = uin("Age:");
+            userTicket[i].age= tryStrToInt(strAge);
+            //User's Destination City
+            userTicket[i].destinationCity = uin("Destination City:");
+            //User's Destination State
+            do{
+                userTicket[i].destinationState = uin("Destination State(Valid 2-letter Postal Code):");
+            }while(userTicket[i].destinationState.length() > 2 ||
+                    !userTicket[i].destinationState.equalsIgnoreCase("AL") && 
+                    !userTicket[i].destinationState.equalsIgnoreCase("AK") && 
+                    !userTicket[i].destinationState.equalsIgnoreCase("AZ") && 
+                    !userTicket[i].destinationState.equalsIgnoreCase("AR") && 
+                    !userTicket[i].destinationState.equalsIgnoreCase("CA") && 
+                    !userTicket[i].destinationState.equalsIgnoreCase("CO") && 
+                    !userTicket[i].destinationState.equalsIgnoreCase("CT") && 
+                    !userTicket[i].destinationState.equalsIgnoreCase("DE") && 
+                    !userTicket[i].destinationState.equalsIgnoreCase("DC") && 
+                    !userTicket[i].destinationState.equalsIgnoreCase("FL") && 
+                    !userTicket[i].destinationState.equalsIgnoreCase("GA") && 
+                    !userTicket[i].destinationState.equalsIgnoreCase("HI") &&
+                    !userTicket[i].destinationState.equalsIgnoreCase("IA") &&
+                    !userTicket[i].destinationState.equalsIgnoreCase("ID") && 
+                    !userTicket[i].destinationState.equalsIgnoreCase("IN") &&
+                    !userTicket[i].destinationState.equalsIgnoreCase("KS") && 
+                    !userTicket[i].destinationState.equalsIgnoreCase("KY") && 
+                    !userTicket[i].destinationState.equalsIgnoreCase("LA") && 
+                    !userTicket[i].destinationState.equalsIgnoreCase("ME") && 
+                    !userTicket[i].destinationState.equalsIgnoreCase("MD") && 
+                    !userTicket[i].destinationState.equalsIgnoreCase("MA") && 
+                    !userTicket[i].destinationState.equalsIgnoreCase("MI") && 
+                    !userTicket[i].destinationState.equalsIgnoreCase("MN") && 
+                    !userTicket[i].destinationState.equalsIgnoreCase("MS") && 
+                    !userTicket[i].destinationState.equalsIgnoreCase("MO") && 
+                    !userTicket[i].destinationState.equalsIgnoreCase("MT") && 
+                    !userTicket[i].destinationState.equalsIgnoreCase("NE") && 
+                    !userTicket[i].destinationState.equalsIgnoreCase("NV") && 
+                    !userTicket[i].destinationState.equalsIgnoreCase("NH") && 
+                    !userTicket[i].destinationState.equalsIgnoreCase("NJ") &&
+                    !userTicket[i].destinationState.equalsIgnoreCase("NM") &&
+                    !userTicket[i].destinationState.equalsIgnoreCase("NY") &&
+                    !userTicket[i].destinationState.equalsIgnoreCase("NC") &&
+                    !userTicket[i].destinationState.equalsIgnoreCase("NC") &&
+                    !userTicket[i].destinationState.equalsIgnoreCase("ND") &&
+                    !userTicket[i].destinationState.equalsIgnoreCase("OH") && 
+                    !userTicket[i].destinationState.equalsIgnoreCase("OK") && 
+                    !userTicket[i].destinationState.equalsIgnoreCase("OR") &&
+                    !userTicket[i].destinationState.equalsIgnoreCase("PA") &&
+                    !userTicket[i].destinationState.equalsIgnoreCase("RI") && 
+                    !userTicket[i].destinationState.equalsIgnoreCase("SC") &&
+                    !userTicket[i].destinationState.equalsIgnoreCase("SD") &&
+                    !userTicket[i].destinationState.equalsIgnoreCase("TN") &&
+                    !userTicket[i].destinationState.equalsIgnoreCase("TX") &&
+                    !userTicket[i].destinationState.equalsIgnoreCase("UT") &&
+                    !userTicket[i].destinationState.equalsIgnoreCase("VT") &&
+                    !userTicket[i].destinationState.equalsIgnoreCase("VA") &&
+                    !userTicket[i].destinationState.equalsIgnoreCase("WA") && 
+                    !userTicket[i].destinationState.equalsIgnoreCase("WV") &&
+                    !userTicket[i].destinationState.equalsIgnoreCase("WI") && 
+                    !userTicket[i].destinationState.equalsIgnoreCase("WY"));
+            //User's Class Choice
+            int classChoice = 0;
+            do{
+                userTicket[i].classChoice = uin("Class:\n1. First\n2. Economy\n");
+                classChoice = tryStrToInt(userTicket[i].classChoice);
+            }while(classChoice < 1 || classChoice > 2);
+            switch(classChoice){
+                case 1:
+                    userTicket[i].classChoice = "First Class";
+                    userTicket[i].seat = firstSeat();
+                    break;
+                case 2:
+                    userTicket[i].classChoice = "Economy Class";
+                    userTicket[i].seat = economySeat(userTicket[i].age);
+                    break;
+                default:
+                    userTicket[i].classChoice = "Economy Class";
+                    userTicket[i].seat = economySeat(userTicket[i].age);
+                    break;
+            }
+            //Military
+            System.out.println("Is the flight Military realated?(y/n)");
+            String strMilitary = input.nextLine();
+            if(strMilitary.equalsIgnoreCase("y") || strMilitary.equalsIgnoreCase("yes")){
+                userTicket[i].military = "Yes";
+            }
+            else if(strMilitary.equalsIgnoreCase("n") || strMilitary.equalsIgnoreCase("no")){
+                userTicket[i].military = "No";
+            }
+            else{
+                userTicket[i].military = "No";
+            }
+            //Disablity
+            System.out.println("Do you have a physical disability?(y/n)");
+            String strDisability = input.nextLine();
+            if(strDisability.equalsIgnoreCase("y") || strDisability.equalsIgnoreCase("yes")){
+                userTicket[i].disability = "Yes";
+            }
+            else if(strDisability.equalsIgnoreCase("n") || strDisability.equalsIgnoreCase("no")){
+                userTicket[i].disability = "No";
+            }
+            else{
+                userTicket[i].disability = "No";
+            }
+            printAll(userTicket[i].ticketNum, userTicket[i].lastName, userTicket[i].firstName, userTicket[i].middleInitial, userTicket[i].age, userTicket[i].destinationCity, userTicket[i].destinationState, userTicket[i].classChoice, userTicket[i].seat, userTicket[i].military, userTicket[i].disability);
+            ticketNum++;
         }
-        //Military
-        System.out.println("Is the flight Military realated?(y/n)");
-        String strMilitary = input.nextLine();
-        boolean military;
-        if (strMilitary.equalsIgnoreCase("y") || strMilitary.equalsIgnoreCase("yes")){
-          military = true;
-          strMilitary = "Yes";
-        }
-        else if (strMilitary.equalsIgnoreCase("n") || strMilitary.equalsIgnoreCase("no")){
-          military = false;
-          strMilitary = "No";
-        }
-        else{
-          military = false;
-          strMilitary = "No";
-        }
-        //Disablity
-        System.out.println("Do you have a physical disability?(y/n)");
-        String strDisability = input.nextLine();
-        boolean disability;
-        if (strDisability.equalsIgnoreCase("y") || strDisability.equalsIgnoreCase("yes")){
-          disability = true;
-          strDisability = "Yes";
-        }
-        else if (strDisability.equalsIgnoreCase("n") || strDisability.equalsIgnoreCase("no")){
-          disability = false;
-          strDisability = "No";
-        }
-        else{
-          disability = false;
-          strDisability = "No";
-        }
-        //Print Information
-        printAll(lastName, firstName, middleInitial, destinationCity, destinationState, strClassChoice, seat, strMilitary, strDisability);
     }
     public static String uin(String prompt){
         String uin;
@@ -104,13 +157,20 @@ public class AirplaneMidterm {
         String seat = strRowNum+seatLetter;
         return seat;
     }
-    public static String economySeat(){
+    public static String economySeat(int age){
         String strRowNum;
         int rowNum;
         do{
             System.out.println("Select a row between 7 and 33: ");
             strRowNum = input.nextLine();
             rowNum = tryStrToInt(strRowNum);
+            if (age < 15){
+                do{
+                    System.out.println("Children under the age of 15 cannot sit in the Exit rows. Please enter a new row number");
+                    strRowNum = input.nextLine();
+                    rowNum = tryStrToInt(strRowNum);
+                }while(rowNum == 15 || rowNum == 14);
+            }
         }while(rowNum < 7 || rowNum > 33);
         String seatLetter;
         do{
@@ -130,7 +190,7 @@ public class AirplaneMidterm {
         }
         return cin;
     }
-    public static void printAll(String lastName, String firstName, String middleInitial, String destinationCity, String destinationState, String strClassChoice, String seat, String strMilitary, String strDisability){
-        System.out.printf("================================================\nFull Name: %s, %s %s\nDestination: %s, %s\nClass Choice: %s\nSeat: %s\nMilitary?: %s\nDisability?: %s\n", lastName, firstName, middleInitial, destinationCity, destinationState, strClassChoice, seat, strMilitary, strDisability);
+    public static void printAll(int ticketNum, String lastName, String firstName, String middleInitial, int age, String destinationCity, String destinationState, String strClassChoice, String seat, String strMilitary, String strDisability){
+        System.out.printf("================================================\nTicket Number: %d\nFull Name: %s, %s %s\nAge: %d\nDestination: %s, %s\nClass Choice: %s\nSeat: %s\nMilitary?: %s\nDisability?: %s\n", ticketNum, lastName, firstName, middleInitial, age, destinationCity, destinationState, strClassChoice, seat, strMilitary, strDisability);
     }
 }
--- Ticket.java
+++ Ticket.java
@@ -1,11 +1,93 @@
 package airplane.midterm;
 public class Ticket {
-    String firstName;
-    String middleInitial;
-    String lastName;
-    String destinationCity;
-    String destinationState;
-    String seat;
-    boolean miliatry;
-    boolean disability;
+    public int ticketNum;
+    public String firstName;
+    public String middleInitial;
+    public String lastName;
+    public int age;
+    public String destinationCity;
+    public String destinationState;
+    public String classChoice;
+    public String military;
+    public String disability;
+    public String seat;
+    public void ticket(int ticketNum, String firstName, String middleInitial, String lastName, int age, String destinationCity, String destinationState, String classChoice, String seat, String military, String disability){
+        setTicketNum(ticketNum);
+        setFirstName(firstName);
+        setMiddleInitial(middleInitial);
+        setLastName(lastName);
+        setAge(age);
+        setDestinationCity(destinationCity);
+        setDestinationState(destinationState);
+        setClassChoice(classChoice);
+        setSeat(seat);
+        setMilitary(military);
+        setDisability(disability);
+    }
+    public int getTicketNum(){
+        return ticketNum;
+    }
+    public String getFirstName(){
+        return firstName;
+    }
+    public String getMiddleInitial(){
+        return middleInitial;
+    }
+    public String getLastName(){
+        return lastName;
+    }
+    public int getAge(){
+        return age;
+    }
+    public String getDestinationCity(){
+        return destinationCity;
+    }
+    public String getDestinationState(){
+        return destinationState;
+    }
+    public String getClassChoice(){
+        return classChoice;
+    }
+        public String getSeat(){
+        return seat;
+    }
+    public String getMilitary(){
+        return military;
+    }
+    public String getDisability(){
+        return disability;
+    }
+    public void setTicketNum(int ticketNum){
+        this.ticketNum = ticketNum;
+    }
+    public void setFirstName(String firstName){
+        this.firstName = firstName;
+    }
+    public void setMiddleInitial(String middleInitial){
+        this.middleInitial = middleInitial;
+    }
+    public void setLastName(String lastName){
+        this.lastName = lastName;
+    }
+    public void setAge(int age){
+        this.age = age;
+    }
+    public void setDestinationCity(String destinationCity){
+        this.destinationCity = destinationCity;
+    }
+    public void setDestinationState(String destinationState){
+        this.destinationState = destinationState;
+    }
+    public void setClassChoice(String classChoice){
+        this.classChoice = classChoice;
+    }
+    public void setSeat(String seat){
+        this.seat = seat;
+    }
+    public void setMilitary(String military){
+        this.military = military;
+    }
+    public void setDisability(String disability){
+        this.disability = disability;
+    }
 }
