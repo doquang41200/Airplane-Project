89b03c67986358517d97a781c9a29a073722aa64
petersg29 <57017091+petersg29@users.noreply.github.com>
2019-10-30 21:00:36 -0500

    Add files via upload
    I think i got it

---
+++ airline.java
@@ -0,0 +1,13 @@
+package airline;
+
+import java.io.File;
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+public class airline {
+	private static final int Flights = 50;
+	private static final String PATH = "StateFlightTimes.csv";
+
+	private static final String[] STATES= new String[ Flights ];
+	private static final Float[]  FLIGHTTIME = new Float[Flights];
+}
