c033c5d9b3c97570875399dd185fd5ecb9a6580a
codeman2525 <54909226+codeman2525@users.noreply.github.com>
2019-10-30 19:27:47 -0500

    Update Receipit.java

--- Receipit.java
+++ Receipit.java
@@ -1,4 +1,38 @@
-
 public class Receipit {
+	public String firstName;
+	public String middleInitial;
+	public String lastName;
+	public int numOfTickets;
+	
+	
+	public Receipit(String firstName, String middleInitial, String lastName, int numTickets, price?) {
+		this.firstName=firstName;
+		this.middleInitial=middleInitial;
+		this.lastName=lastName;
+		this.numOfTickets=numOfTickets;
+		this.Price=Price;
+	}
 
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+
+	public String getMiddleInitial() {
+		return middleInitial;
+	}
+
+
+	public String getLastName() {
+		return lastName;
+	}
+
+
+	public int getNumOfTickets() {
+		return numOfTickets;
+		
+	public double getAddOnPrice() {
+		return addOnPrice;
+	}
 }
